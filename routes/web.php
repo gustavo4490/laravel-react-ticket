bbbfe0f556cc9743d9306315c0b15832ed4a9230
Andres <andresf150@gmail.com>
2025-04-07 17:24:06 -0600

    agregar rutas para la funcionalidad CRUD de clientes, soporte y tickets

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,29 @@
 <?php
 
+use App\Models\Customer;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\SupportController;
+
 
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::get('/about', function () {
+    return Inertia::render('about');
+})->name('about');
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::resource('customers', CustomerController::class);
+    Route::resource('support', SupportController::class);
+    Route::resource('tickets', TicketController::class);
 });
 
 require __DIR__.'/settings.php';
