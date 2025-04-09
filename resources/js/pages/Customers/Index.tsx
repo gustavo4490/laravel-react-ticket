af4cbb55d03f1bb1c64d0ee9ba63c11695c465c9
Andres <andresf150@gmail.com>
2025-04-09 16:57:41 -0600

    agregar componente DataTable y actualizar página de clientes para mostrar datos en formato de tabla

--- resources/js/pages/Customers/Index.tsx
+++ resources/js/pages/Customers/Index.tsx
@@ -1,3 +1,4 @@
+import { DataTable } from '@/components/ui/dataTable';
 import AppLayout from '@/layouts/app-layout';
 import { CustomersProps, type BreadcrumbItem } from '@/types';
 import { Head, usePage } from '@inertiajs/react';
@@ -9,6 +10,29 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+const columns = [
+    {
+        accessorKey: 'id',
+        header: 'ID',
+    },
+    {
+        accessorKey: 'name',
+        header: 'Nombre',
+    },
+    {
+        accessorKey: 'email',
+        header: 'Email',
+    },
+    {
+        accessorKey: 'phone',
+        header: 'Teléfono',
+    },
+    {
+        accessorKey: 'address',
+        header: 'Dirección',
+    },
+];
+
 export default function Index() {
     const { customers } = usePage<CustomersProps>().props;
 
@@ -17,13 +41,7 @@ export default function Index() {
             <Head title="Clientes" />
             {/* contenedor principal   */}
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <ul>
-                    {customers.data.map((customer) => (
-                        <li key={customer.id} className="border-b border-gray-200 py-2">
-                            {customer.name} - {customer.email} {customer.name}
-                        </li>
-                    ))}
-                </ul>
+                <DataTable columns={columns} data={customers.data} />
             </div>
         </AppLayout>
     );
